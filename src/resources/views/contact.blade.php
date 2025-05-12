ec12f20f7bbc378bd867c6a2ddebcf9090fb7194
endou9527 <kd2527ar5p@gmail.com>
2025-05-13 00:22:41 +0900

    bladeファイルの編集

--- src/resources/views/contact.blade.php
+++ src/resources/views/contact.blade.php
@@ -34,7 +34,7 @@
     <div class="form-group">
       <label class="form-label">性別 <span class="required">※</span></label>
       <div class="form-control">
-        <div class="radio-group">
+        <div class="gender-fields">
           <label><input type="radio" name="gender" value="男性" {{ old('gender')  == '男性' ? 'checked': '' }}> 男性</label>
           <label><input type="radio" name="gender" value="女性" {{ old('gender')  == '女性' ? 'checked': '' }}> 女性</label>
           <label><input type="radio" name="gender" value="その他" {{ old('gender')  == 'その他' ? 'checked': '' }}> その他</label>
@@ -50,7 +50,9 @@
 
     <div class="form-group">
       <label class="form-label">メールアドレス <span class="required">※</span></label>
-      <input type="email" name="email" value="{{ old('email') }}" placeholder="例: test@example.com">
+      <div class="email-fields">
+        <input type="email" name="email" value="{{ old('email') }}" placeholder="例: test@example.com">
+      </div>
       <div class="form__error">
           @error('email')
           {{ $message }}
@@ -80,7 +82,9 @@
 
     <div class="form-group">
       <label class="form-label">住所 <span class="required">※</span></label>
-      <input type="text" name="address" value="{{ old('address') }}"  placeholder="例: 東京都渋谷区千駄ヶ谷1-2-3">
+      <div class="address-fields">
+        <input type="text" name="address" value="{{ old('address') }}"  placeholder="例: 東京都渋谷区千駄ヶ谷1-2-3">
+      </div>
     </div>
     <div class="form__error">
           @error('address')
@@ -90,19 +94,23 @@
 
     <div class="form-group">
       <label class="form-label">建物名</label>
-      <input type="text" name="address__building" value="{{ old('address__building') }}" placeholder="例: 千駄ヶ谷マンション101">
+      <div class="building-fields">
+        <input type="text" name="address__building" value="{{ old('address__building') }}" placeholder="例: 千駄ヶ谷マンション101">
+      </div>
     </div>
 
     <div class="form-group">
       <label class="form-label">お問い合わせの種類 <span class="required">※</span></label>
-      <select name="category_id">
-        <option value="">選択してください</option>
-        <option value="1. 商品のお届けについて" {{ old('category_id')== '1. 商品のお届けについて' ? 'selected' : '' }}>1. 商品のお届けについて</option>
-        <option value="2. 商品の交換について" {{ old('category_id')== '2. 商品の交換について' ? 'selected' : '' }}>2. 商品の交換について</option>
-        <option value="3. 商品トラブル" {{ old('category_id')== '3. 商品トラブル' ? 'selected' : '' }}>3. 商品トラブル</option>
-        <option value="4. ショップへのお問い合わせ" {{ old('category_id')== '4. ショップへのお問い合わせ' ? 'selected' : '' }}>4. ショップへのお問い合わせ</option>
-        <option value="5. その他" {{ old('category_id')== '5. その他' ? 'selected' : '' }}>5. その他</option>
-      </select>
+      <div class="category-fields">
+       <select name="category_id">
+          <option value="">選択してください</option>
+          <option value="1. 商品のお届けについて" {{ old('category_id')== '1. 商品のお届けについて' ? 'selected' : '' }}>1. 商品のお届けについて</option>
+          <option value="2. 商品の交換について" {{ old('category_id')== '2. 商品の交換について' ? 'selected' : '' }}>2. 商品の交換について</option>
+          <option value="3. 商品トラブル" {{ old('category_id')== '3. 商品トラブル' ? 'selected' : '' }}>3. 商品トラブル</option>
+          <option value="4. ショップへのお問い合わせ" {{ old('category_id')== '4. ショップへのお問い合わせ' ? 'selected' : '' }}>4. ショップへのお問い合わせ</option>
+          <option value="5. その他" {{ old('category_id')== '5. その他' ? 'selected' : '' }}>5. その他</option>
+        </select> 
+      </div>
     </div>
     <div class="form__error">
           @error('category_id')
@@ -112,7 +120,10 @@
 
     <div class="form-group">
       <label class="form-label">お問い合わせ内容 <span class="required">※</span></label>
-      <textarea name="detail" placeholder="お問い合わせ内容をご記載ください">{{ old('detail') }}</textarea>
+      <div class="detail-fields">
+        <textarea name="detail" placeholder="お問い合わせ内容をご記載ください">{{ old('detail') }}</textarea>
+      </div>
+      
     </div>
     <div class="form__error">
           @error('detail')
